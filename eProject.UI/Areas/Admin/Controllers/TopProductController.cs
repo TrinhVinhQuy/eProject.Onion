09f431f0e30d72df7e1ab22eaedf1b25aa3afaba
VinhQuy1402 <vinhquytrinh@gmail.com>
2024-04-29 15:27:14 +0700

    up

---
+++ eProject.UI/Areas/Admin/Controllers/TopProductController.cs
@@ -0,0 +1,28 @@
+﻿using eProject.Application.Abstracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProject.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class TopProductController : Controller
+    {
+        private readonly IProductServices _productsService;
+        public TopProductController(IProductServices productsService)
+        {
+            _productsService = productsService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/admin/top-product")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _productsService.GetAllAsync();
+            result = result.OrderByDescending(x => x.SoldItem);
+            return Ok(result);
+        }
+    }
+}
